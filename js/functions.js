51d134e758ec1efe8b3a567272f12d58640ed04f
Dillon Carns <dcarns72@gmail.com>
2019-01-21 14:48:13 -0500

    added contact functionality

--- js/functions.js
+++ js/functions.js
@@ -1,59 +1,30 @@
-
-function emailMe() {
-	var x = document.getElementById("email_form");
-	var createForm = document.createElement('form');
-	createform.setAttribute("action", "");
-	createform.setAttribute("method", "post");
-	x.appendChild(createForm);
-	
-	var heading = document.createElement('h2'); // Heading of Form
-	heading.innerHTML = "Contact Form ";
-	createform.appendChild(heading);
-	
-	var line = document.createElement('hr'); // Giving Horizontal Row After Heading
-	createform.appendChild(line);
-	
-	var linebreak = document.createElement('br');
-	createform.appendChild(linebreak);
-	
-	var namelabel = document.createElement('label'); // Create Label for Name Field
-	namelabel.innerHTML = "Your Name : "; // Set Field Labels
-	createform.appendChild(namelabel);
-	
-	var inputelement = document.createElement('input'); // Create Input Field for Name
-	inputelement.setAttribute("type", "text");
-	inputelement.setAttribute("name", "dname");
-	createform.appendChild(inputelement);
-	
-	var linebreak = document.createElement('br');
-	createform.appendChild(linebreak);
-	
-	var emaillabel = document.createElement('label'); // Create Label for E-mail Field
-	emaillabel.innerHTML = "Your Email : ";
-	createform.appendChild(emaillabel);
-	
-	var emailelement = document.createElement('input'); // Create Input Field for E-mail
-	emailelement.setAttribute("type", "text");
-	emailelement.setAttribute("name", "demail");
-	createform.appendChild(emailelement);
-	
-	var emailbreak = document.createElement('br');
-	createform.appendChild(emailbreak)
-	
-	var messagelabel = document.createElement('label'); // Append Textarea
-	messagelabel.innerHTML = "Your Message : ";
-	createform.appendChild(messagelabel);
-
-	var texareaelement = document.createElement('textarea');
-	texareaelement.setAttribute("name", "dmessage");
-	createform.appendChild(texareaelement);
-
-	var messagebreak = document.createElement('br');
-	createform.appendChild(messagebreak);
-
-	var submitelement = document.createElement('input'); // Append Submit Button
-	submitelement.setAttribute("type", "submit");
-	submitelement.setAttribute("name", "dsubmit");
-	submitelement.setAttribute("value", "Submit");
-	createform.appendChild(submitelement);
-}
+$(document).ready(function () {
+	$('.submit').click(function(event) {
+		var email = $('.email').val()
+		var subject = $('.subject').val()
+		var message = $('.msg').val()
+		var statusElm = $('.status')
+		statusElm.empty();
+		
+		if (email.length > 5 && email.includes('@') && email.includes('.')) {
+				statusElm.append("");
+		} else {
+				statusElm.append("<div>email is not valid</div>");
+				event.preventDefault();
+		}
+		
+		if (subject.length >= 2) {
+			statusElm.append("");
+		} else {
+			statusElm.append("<div>Subject requires atleast 2 characters.</div>");
+			event.preventDefault();
+		}
+		
+		if (message.length >= 5) {
+			statusElm.append("<div>Message has been sent!</div>");
+		} else {
+			statusElm.append("<div>Message requires 5 characters of content.</div>");
+			event.preventDefault();
+		}
+	})
+});
